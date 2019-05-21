706f821b041ed312774c650ceb2d8970e8d2100d
Juan Camilo Sarmiento Reyes <jcamilosarmientor@gmail.com>
2019-05-21 11:15:34 -0500

    retornado entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  # export SEGURIDAD_SOCIAL_CRUD_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/seguridad_social_crud/db/username --output text --query Parameter.Value)"
-  # export SEGURIDAD_SOCIAL_CRUD_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/seguridad_social_crud/db/password --output text --query Parameter.Value)"
-  export SEGURIDAD_SOCIAL_CRUD_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_crud_api/db/username --output text --query Parameter.Value)"
-  export SEGURIDAD_SOCIAL_CRUD_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_crud_api/db/password --output text --query Parameter.Value)"
+  export SEGURIDAD_SOCIAL_CRUD_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/seguridad_social_crud/db/username --output text --query Parameter.Value)"
+  export SEGURIDAD_SOCIAL_CRUD_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/seguridad_social_crud/db/password --output text --query Parameter.Value)"
+  # export SEGURIDAD_SOCIAL_CRUD_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_crud_api/db/username --output text --query Parameter.Value)"
+  # export SEGURIDAD_SOCIAL_CRUD_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_crud_api/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
