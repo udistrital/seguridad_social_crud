72f5656bcf695d3a03529d9d485762f7627aeacc
fernandotower <fernandotower@gmail.com>
2019-05-21 12:17:20 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export SEGURIDAD_SOCIAL_CRUD_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_crud_api/db/username --output text --query Parameter.Value)"
-  export SEGURIDAD_SOCIAL_CRUD_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_crud_api/db/password --output text --query Parameter.Value)"
+  export SS_CRUD_API_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_crud_api/db/username --output text --query Parameter.Value)"
+  export SS_CRUD_API_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_crud_api/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
